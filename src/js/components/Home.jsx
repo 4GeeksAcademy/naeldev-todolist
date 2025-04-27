eee5324bc0cd3352bc7c31a058e818df35ac1aa9
Nael <naeldiaz@proton.me>
2025-04-27 12:00:22 +0000

    va cogiendo color

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -8,30 +8,43 @@ import rigoImage from "../../img/rigo-baby.jpg";
 const Home = () => {
 	const [inputValue, setInputValue] = useState('');
 	const [tasks, setTask] = useState([]);
-	let count =0;
+	let count =tasks.length;
 
 	const handleChange = (e) => {
 		setInputValue(e.target.value);
 		
+		
 	}
-	const addTask =e => {
+	const addTask = (e) => {
+		const limite=10;
 		e.preventDefault();
 		if(inputValue.trim()=== ''){
 			alert('Debe contener un valor')
 			return;
 		}
-		setTask([...tasks, inputValue]);
-		setInputValue('');
-		count = tasks.length+1;
-		console.log(tasks.length+1)
+
+		if(tasks.length < limite){
+			setTask([...tasks, inputValue]);
+			setInputValue('');
+		} else{
+			alert("No puedes tener tantas tareas pendientes, termina o elimina una para agregar otra")
+		}
+		
+		
 	
 	}
-	count = tasks.length;
+	const deleteTask=(index) =>{
+		const newTasks = tasks.filter((_, i) => i !== index);
+        setTask(newTasks);
+		
+	}
+	
+	
 	
 
 
 	return (
-	<>
+		<>
 			<div className=" text-center container-fluid m-5 p-2">
 				<div className=" d-flex justify-content-center ">
 					<h1 className="center">To DoS...</h1>
@@ -42,16 +55,29 @@ const Home = () => {
 					handleChange={handleChange}
 					value={inputValue}
 					addTask={addTask}
-
-
 				/>
 				<div className="mt-3 text-center">
 					{tasks.map((task, index) => (
-						<p key={index} className="border p-2">{task}</p>
+						<p
+							key={index}
+							className="text-center p-2">
+							{task}
+							<button
+								onClick={()=>deleteTask(index)}
+								type="button"
+								className="btn boton float-end ">
+								x
+							</button>
+						</p>
+
+
 					))}
+
+
+
 				</div>
 				<div>
-					{count === 0 ? "No hay tareas pendientes" : `${count} tareas pendientes`}
+					{count === 0 ? "No hay tareas pendientes, añadir tareas" : `${count} tareas pendientes`}
 
 				</div>
 			</div>
--- src/js/components/toDo.jsx
+++ src/js/components/toDo.jsx
@@ -1,16 +1,16 @@
 import React, { useState } from "react";
 
-const Body = ({handleChange, inputValue,addTask,count}) => {
+const Body = ({handleChange, inputValue,addTask}) => {
     return (
         <div className="container-fluid text-center ">
             <form onSubmit={addTask}>
+            
                 <input 
                     type="text"
                     value={inputValue} 
                     placeholder="Ingresa la tarea"
                     onChange={handleChange}
                     className="form-control"
-                
                 />
         
                 
